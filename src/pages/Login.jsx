e5efdf2057d86dd32a1899a83e792f54b78326cd
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-12-13 18:50:03 -0300

    Configurações iniciais Redux

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const minLength = 6;
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      password: 0,
+      disabled: this.state.password > minLength,
+    };
+  }
+
   render() {
+    const { disabled, password } = this.state;
+    const { dispatch } = this.props;
     return (
       <div>
         <label htmlFor="email">
@@ -9,12 +23,39 @@ class Login extends React.Component {
             type="email"
             id="email"
             data-testid="email-input"
+            required
           />
           Email
         </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            id="password"
+            data-testid="password-input"
+            minLength={ 6 }
+            required
+            value={ password }
+          />
+          Password
+        </label>
+        <button
+          type="button"
+          disabled={ disabled }
+          onClick={ () => dispatch() }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(Login);
